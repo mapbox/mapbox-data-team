7f4aab6a2db67c548afefeca377e5228369f74f7
arunasank <aruna@mapbox.com>
2016-05-20 17:08:46 +0530

    Make do with require

---
+++ data/team.js
@@ -0,0 +1,236 @@
+'use strict';
+
+module.exports = {
+    'team': [
+        {
+            'username': 'Aaron Lidman',
+            'uid': '53073',
+            'fname': 'Aaron',
+            'lname': 'Lidman',
+            'fullname': 'Aaron Lidman'
+        },
+        {
+            'username': 'aarthy',
+            'uid': '2985232',
+            'fname': 'Aarthy',
+            'lname': 'Chandrasekhar',
+            'fullname': 'Aarthy Chandrasekhar'
+        },
+        {
+            'username': 'saikabhi',
+            'uid': '3029661',
+            'fname': 'Abhishek',
+            'lname': 'Saikia',
+            'fullname': 'Abhishek Saikia'
+        },
+        {
+            'username': 'lxbarth',
+            'uid': '589596',
+            'fname': 'Alex',
+            'lname': 'Barth',
+            'fullname': 'Alex Barth'
+        },
+        {
+            'username': 'andygol',
+            'uid': '94578',
+            'fname': 'Andrey',
+            'lname': 'Golovin',
+            'fullname': 'Andrey Golovin'
+        },
+        {
+            'username': 'PlaneMad',
+            'uid': '1306',
+            'fname': 'Arun',
+            'lname': 'Ganesh',
+            'fullname': 'Arun Ganesh'
+        },
+        {
+            'username': 'Arunasank',
+            'uid': '2477516',
+            'fname': 'Aruna',
+            'lname': 'Sankaranarayanan',
+            'fullname': 'Aruna Sankaranarayanan'
+        },
+        {
+            'username': 'bkowshik',
+            'uid': '1087876',
+            'fname': 'Bhargav',
+            'lname': 'Kowshik',
+            'fullname': 'Bhargav Kowshik'
+        },
+        {
+            'username': 'calfarome',
+            'uid': '2511706',
+            'fname': 'Cesar',
+            'lname': 'Alfaro',
+            'fullname': 'Cesar Alfaro'
+        },
+        {
+            'username': 'Chetan_Gowda',
+            'uid': '2644101',
+            'fname': 'Chethan',
+            'lname': 'Gowda',
+            'fullname': 'Chethan Gowda'
+        },
+        {
+            'username': 'dannykath',
+            'uid': '2226712',
+            'fname': 'Danny',
+            'mname': 'Aiquipa',
+            'lname': 'Pacheco',
+            'fullname': 'Danny Aiquipa Pacheco'
+        },
+        {
+            'username': 'ediyes',
+            'uid': '1240849',
+            'fname': 'Edith',
+            'lname': 'Quispe',
+            'fullname': 'Edith Quispe'
+        },
+        {
+            'username': 'samely',
+            'uid': '2512300',
+            'fname': 'Eliane',
+            'mname': 'Joyo',
+            'lname': 'Meneses',
+            'fullname': 'Eliane Joyo Meneses'
+        },
+        {
+            'username': 'jinalfoflia',
+            'uid': '2847988',
+            'fname': 'Jinal',
+            'lname': 'Foflia',
+            'fullname': 'Jinal Foflia'
+        },
+        {
+            'username': 'Jothirnadh',
+            'uid': '2015224',
+            'fname': 'Jothirnadh',
+            'lname': 'Sai',
+            'fullname': 'Jothirnadh Sai'
+        },
+        {
+            'username': 'karitotp',
+            'uid': '2748195',
+            'fname': 'Karito',
+            'mname': 'Tenorio',
+            'lname': 'Palomino',
+            'fullname': 'Karito Tenorio Palomino'
+        },
+        {
+            'username': 'Luis36995',
+            'uid': '1829683',
+            'fname': 'Luis',
+            'mname': 'Alanya',
+            'lname': 'Ortega',
+            'fullname': 'Luis Alanya Ortega'
+        },
+        {
+            'username': 'manings',
+            'uid': '3272286',
+            'fname': 'Maning',
+            'lname': 'Sambale',
+            'fullname': 'Maning Sambale'
+        },
+        {
+            'username': 'nammala',
+            'uid': '3479270',
+            'fname': 'Krishna',
+            'lname': 'Nammala',
+            'fullname': 'Krishna Nammala'
+        },
+        {
+            'username': 'nikhilprabhakar',
+            'uid': '2835928',
+            'fname': 'Nikhil',
+            'lname': 'Prabhakar',
+            'fullname': 'Nikhil Prabhakar'
+        },
+        {
+            'username': 'oini',
+            'uid': '3057995',
+            'fname': 'Oindrila',
+            'lname': 'Gupta',
+            'fullname': 'Oindrila Gupta'
+        },
+        {
+            'username': 'ridixcr',
+            'uid': '2508151',
+            'fname': 'Pavel',
+            'lname': 'Baez',
+            'fullname': 'Pavel Baez'
+        },
+        {
+            'username': 'poornibadrinath',
+            'uid': '1597155',
+            'fname': 'Poornima',
+            'lname': 'Badrinath',
+            'fullname': 'Poornima Badrinath'
+        },
+        {
+            'username': 'pratikyadav',
+            'uid': '2905914',
+            'fname': 'Pratik',
+            'lname': 'Yadav',
+            'fullname': 'Pratik Yadav'
+        },
+        {
+            'username': 'ramyaragupathy',
+            'uid': '2823295',
+            'fname': 'Ramya',
+            'lname': 'Ragupathy',
+            'fullname': 'Ramya Ragupathy'
+        },
+        {
+            'username': 'RichRico',
+            'uid': '2219338',
+            'fname': 'Richman',
+            'mname': 'Pariona',
+            'lname': 'Torres',
+            'fullname': 'Richman Pariona Torres'
+        },
+        {
+            'username': 'Rub21',
+            'uid': '510836',
+            'fname': 'Ruben',
+            'mname': 'Lopez',
+            'lname': 'Mendoza',
+            'fullname': 'Ruben Lopez Mendoza'
+        },
+        {
+            'username': 'ruthmaben',
+            'uid': '2554698',
+            'fname': 'Ruth',
+            'lname': 'Maben',
+            'fullname': 'Ruth Maben'
+        },
+        {
+            'username': 'geohacker',
+            'uid': '146675',
+            'fname': 'Sajjad',
+            'lname': 'Anwar',
+            'fullname': 'Sajjad Anwar'
+        },
+        {
+            'username': 'sanjayb',
+            'uid': '261012',
+            'fname': 'Sanjay',
+            'lname': 'Bhangar',
+            'fullname': 'Sanjay Bhangar'
+        },
+        {
+            'username': 'shvrm',
+            'uid': '2306749',
+            'fname': 'Sivaram',
+            'lname': 'Ramachandran',
+            'fullname': 'Sivaram Ramachandran'
+        },
+        {
+            'username': 'srividya_c',
+            'uid': '2115749',
+            'fname': 'Srividya',
+            'lname': 'Bharadwaj',
+            'fullname': 'Srividya Bharadwaj'
+        }
+    ]
+};
