6f70477995335287ed98ebbfe7f88f062222dfd7
arunasank <aruna@mapbox.com>
2016-05-20 17:20:28 +0530

    All methods

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 'use strict';
-var _ = require('underscore');
-var jsonfile = require('jsonfile');
 
-var team = _.extend({}, jsonfile.readFileSync('data/team.json'));
+var team = require('./data/team.js').team;
+
+function getData(returnField, searchField, value) {
+    if (!searchField) {
+        return team.map(function (member) {
+            return member[returnField];
+        }).sort();
+    } else {
+        return team.filter(function (member) {
+            return (member[searchField] === value);
+        }).map(function (member) {
+            return member[returnField];
+        }).sort();
+    }
+}
+
+function getUsernames() {
+    return getData('username');
+}
+
+function getUserIds() {
+    return getData('uid');
+}
+
+function getUsernamesFor(field, value) {
+    return getData('username', field, value);
+}
+
+function getUserIdsFor(field, value) {
+    return getData('uid', field, value);
+}
+
+function getNames() {
+    return getData('fullname');
+}
+
+function getEverything() {
+    return team;
+}
+
+module.exports = {
+    'getUsernames': getUsernames,
+    'getNames': getNames,
+    'getUserIds': getUserIds,
+    'getUsernamesFor': getUsernamesFor,
+    'getUserIdsFor': getUserIdsFor,
+    'getEverything': getEverything
+};
