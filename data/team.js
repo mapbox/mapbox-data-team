d3a653b4f64a6ffdff4ddace8579df66c6871e50
jinalfoflia <jinal@mapbox.com>
2018-09-10 20:11:23 +0530

    cleaning up the list

--- data/team.js
+++ data/team.js
@@ -30,26 +30,6 @@ module.exports = {
             'github': 'aarthykc'
         },
         {
-            'username': 'saikabhi',
-            'uid': '3029661',
-            'fname': 'Abhishek',
-            'lname': 'Saikia',
-            'fullname': 'Abhishek Saikia',
-            'github': 'abhisheksaikia',
-            'other_accounts': [
-                {'username': 'saikabhi_sfimport', 'uid': '4893098'},
-                {'username': 'saikabhi_LA_imports', 'uid': '4221399'}
-            ]
-        },
-        {
-            'username': 'ajithranka',
-            'uid': '3878839',
-            'fname': 'Ajith',
-            'lname': 'Ranka',
-            'fullname': 'Ajith Ranka',
-            'github': 'ajithranka'
-        },
-        {
             'username': 'lxbarth',
             'uid': '589596',
             'fname': 'Alex',
@@ -105,85 +85,6 @@ module.exports = {
             ]
         },
         {
-            'username': 'bkowshik',
-            'uid': '1087876',
-            'fname': 'Bhargav',
-            'lname': 'Kowshik',
-            'fullname': 'Bhargav Kowshik',
-            'github': 'bkowshik'
-        },
-        {
-            'username': 'calfarome',
-            'uid': '2511706',
-            'fname': 'Cesar',
-
-            'lname': 'Alfaro',
-            'fullname': 'Cesar Alfaro',
-            'github': 'calfarome',
-            'other_accounts': [
-                {'username': 'calfarome_labuilding', 'uid': '3851691'}
-            ]
-        },
-        {
-            'username': 'Chetan_Gowda',
-            'uid': '2644101',
-            'fname': 'Chethan',
-            'lname': 'Gowda',
-            'fullname': 'Chethan Gowda',
-            'github': 'chtnha',
-            'other_accounts': [
-                {'uid': '3845061', 'username': 'chetan_labuildings'},
-                {'uid': '4884078', 'username': 'chetan-sfimport'}
-            ]
-        },
-        {
-            'username': 'dannykath',
-            'uid': '2226712',
-            'fname': 'Danny',
-            'mname': 'Aiquipa',
-            'lname': 'Pacheco',
-            'fullname': 'Danny Aiquipa Pacheco',
-            'github': 'dannykath',
-            'other_accounts': [
-                {'username': 'dannykath_labuildings', 'uid': '3851440'}
-            ]
-        },
-        {
-            'username': 'ediyes',
-            'uid': '1240849',
-            'fname': 'Edith',
-            'lname': 'Quispe',
-            'fullname': 'Edith Quispe',
-            'github': 'ediyes',
-            'other_accounts': [
-                {'username': 'ediyes_labuilding', 'uid': '3851566'}
-            ]
-        },
-        {
-            'username': 'samely',
-            'uid': '2512300',
-            'fname': 'Eliane Sammy',
-            'mname': 'Joyo',
-            'lname': 'Meneses',
-            'fullname': 'Eliane Sammy Joyo Meneses',
-            'github': 'samely',
-            'other_accounts': [
-                {'username': 'samely_labuildings', 'uid': '3851545'}
-            ]
-        },
-        {
-            'username': 'yurasi',
-            'uid': '3526564',
-            'fname': 'Galia',
-            'mname': 'Eyzaguirre',
-            'lname': 'Luque',
-            'fullname': 'Galia Eyzaguirre Luque',
-            'github': 'yurasi',
-            'other_accounts': [
-                {'username': 'yurasi_import', 'uid': '4210536'}
-            ]
-        },
-        {
             'username': 'Fa7C0N',
             'uid': '4148813',
             'fname': 'Gokul',
@@ -203,41 +104,6 @@ module.exports = {
             'github': 'jinalfoflia',
             'other_accounts': [
                 {'username': 'jinalfoflia_labuilding', 'uid': '3845052'}
-            ]
-        },
-        {
-            'username': 'Jothirnadh',
-            'uid': '2015224',
-            'fname': 'Jothirnadh',
-            'lname': 'Sai',
-            'fullname': 'Jothirnadh Sai',
-            'github': 'jothirnadh',
-            'other_accounts': [
-                {'username': 'Jothirnadh_labuildings', 'uid': '3592311'}
-            ]
-        },
-        {
-            'username': 'karitotp',
-            'uid': '2748195',
-            'fname': 'Karito',
-            'mname': 'Tenorio',
-            'lname': 'Palomino',
-            'fullname': 'Karito Tenorio Palomino',
-            'github': 'karitotp',
-            'other_accounts': [
-                {'username': 'karitotp_labuildings', 'uid': '3851499'}
-            ]
-        },
-        {
-            'username': 'Luis36995',
-            'uid': '1829683',
-            'fname': 'Luis',
-            'mname': 'Alanya',
-            'lname': 'Ortega',
-            'fullname': 'Luis Alanya Ortega',
-            'github': 'luiswalter',
-            'other_accounts': [
-                {'username': 'Luis36995_labuildings', 'uid': '3851527'}
             ]
         },
         {
@@ -264,127 +130,12 @@ module.exports = {
             ]
         },
         {
-            'username': 'nammala',
-            'uid': '3479270',
-            'fname': 'Krishna',
-            'lname': 'Nammala',
-            'fullname': 'Krishna Nammala',
-            'github': 'krishnanammala',
-            'other_accounts': [
-                {'uid': '4222212', 'username': 'nammala_labuildings'}
-            ]
-        },
-        {
-            'username': 'MichaelSteffen',
-            'uid': '2488002',
-            'fname': 'Michael',
-            'lname': 'Steffen',
-            'fullname': 'Michael Steffen',
-            'github': 'michaelsteffen'
-        },
-        {
             'username': 'mikelmaron',
             'uid': '999',
             'fname': 'Mikel',
             'lname': 'Maron',
             'fullname': 'Mikel Maron',
             'github': 'mikelmaron'
-        },
-        {
-            'username': 'nikhilprabhakar',
-            'uid': '2835928',
-            'fname': 'Nikhil',
-            'lname': 'Prabhakar',
-            'fullname': 'Nikhil Prabhakar',
-            'github': 'nikhilprabhakar',
-            'other_accounts': [
-                {'username': 'nikhil_labuildings', 'uid': '3845071'}
-            ]
-        },
-        {
-            'username': 'oini',
-            'uid': '3057995',
-            'fname': 'Oindrila',
-            'lname': 'Gupta',
-            'fullname': 'Oindrila Gupta',
-            'github': 'oini'
-        },
-        {
-            'username': 'ridixcr',
-            'uid': '2508151',
-            'fname': 'Pavel',
-            'lname': 'Baez',
-            'fullname': 'Pavel Baez',
-            'github': 'ridixcr',
-            'other_accounts': [
-                {'username': 'ridixcr_import', 'uid': '4046988'}
-            ]
-        },
-        {
-            'username': 'piligab',
-            'uid': '3877019',
-            'fname': 'Pilar Gabriela',
-            'mname': 'Serrano',
-            'lname': 'Quispe',
-            'fullname': 'Pilar Gabriela Serrano Quispe',
-            'github': 'piligab',
-            'other_accounts': [
-                {'username': 'piligab_labuildings', 'uid': '4210501'}
-            ]
-        },
-        {
-            'username': 'poornibadrinath',
-            'uid': '1597155',
-            'fname': 'Poornima',
-            'lname': 'Badrinath',
-            'fullname': 'Poornima Badrinath',
-            'github': 'poornibadrinath',
-            'other_accounts': [
-                {'username': 'poornima_labuildings', 'uid': '4213767'}
-            ]
-        },
-        {
-            'username': 'pratikyadav',
-            'uid': '2905914',
-            'fname': 'Pratik',
-            'lname': 'Yadav',
-            'fullname': 'Pratik Yadav',
-            'github': 'pratikyadav'
-        },
-        {
-            'username': 'ramyaragupathy',
-            'uid': '2823295',
-            'fname': 'Ramya',
-            'lname': 'Ragupathy',
-            'fullname': 'Ramya Ragupathy',
-            'github': 'ramyaragupathy',
-            'other_accounts': [
-                {'username': 'ramyaragupathy_labuildings', 'uid': '3850292'},
-                {'username': 'ramyaragupathy_sfimport', 'uid': '4896980'},
-            ]
-        },
-        {
-            'username': 'RichRico',
-            'uid': '2219338',
-            'fname': 'Richman',
-            'mname': 'Pariona',
-            'lname': 'Torres',
-            'fullname': 'Richman Pariona Torres',
-            'github': 'RichRico',
-            'other_accounts': [
-                {'username': 'RichRico_labuildings', 'uid': '3851538'}
-            ]
-        },
-        {
-            'username': 'Rub21',
-            'uid': '510836',
-            'fname': 'Ruben',
-            'lname': 'Lopez Mendoza',
-            'fullname': 'Ruben Lopez Mendoza',
-            'github': 'rub21',
-            'other_accounts': [
-                {'username': 'Rub21_labuildings', 'uid': '3851512'}
-            ]
         },
         {
             'username': 'ruthmaben',
@@ -398,96 +149,12 @@ module.exports = {
             ]
         },
         {
-            'username': 'geohacker',
-            'uid': '146675',
-            'fname': 'Sajjad',
-            'lname': 'Anwar',
-            'fullname': 'Sajjad Anwar',
-            'github': 'geohacker'
-        },
-        {
-            'username': 'sanjayb',
-            'uid': '261012',
-            'fname': 'Sanjay',
-            'lname': 'Bhangar',
-            'fullname': 'Sanjay Bhangar',
-            'github': 'batpad'
-        },
-        {
             'username': 'shvrm',
             'uid': '2306749',
             'fname': 'Sivaram',
             'lname': 'Ramachandran',
             'fullname': 'Sivaram Ramachandran',
             'github': 'shvrm'
-        },
-        {
-            'username': 'srividya_c',
-            'uid': '2115749',
-            'fname': 'Srividya',
-            'lname': 'Bharadwaj',
-            'fullname': 'Srividya Bharadwaj',
-            'github': 'srividyacb'
-        },
-        {
-            'username': 'upendrakarukonda',
-            'uid': '4007051',
-            'fname': 'Upendra',
-            'lname': 'Karukonda',
-            'fullname': 'Upendra Karukonda',
-            'github': 'kupendrayadav',
-            'other_accounts': [
-                {'username': 'upendra_labuilding', 'uid': '4517122'},
-                {'username': 'upendra_sfimport', 'uid': '4893041'}
-            ]
-        },
-        {
-            'username': 'Bhojaraj',
-            'uid': '4647004',
-            'fname': 'Bhojaraj',
-            'lname': 'Basavanagowdara',
-            'fullname': 'Bhojaraj Basavangowdara',
-            'github': 'bhojaraj'
-        },
-        {
-            'username': 'ashleyannmathew',
-            'uid': '4994101',
-            'fname': 'Ashley',
-            'lname': 'Mathew',
-            'fullname': 'Ashley Ann Mathew',
-            'github': 'ashleyannmathew'
-        },
-        {
-            'username': 'muziriana',
-            'uid': '6058023',
-            'fname': 'Kelvin',
-            'lname': 'Mathew K',
-            'fullname': 'Kelvin Geo Mathew K',
-            'github': 'muzirian'
-        },
-        {
-            'username': 'bhavana naga',
-            'uid': '5677293',
-            'fname': 'Bhavana',
-            'lname': 'Lingineni',
-            'fullname': 'Bhavana Lingineni',
-            'github': 'bhavana-naga'
-        },
-        {
-            'username': 'marthaleena',
-            'uid': '5659851',
-            'fname': 'Martha',
-            'lname': 'Bonula',
-            'fullname': 'Martha Bonula',
-            'github': 'Marthaleena'
-        },
-        {
-            'username': 'oormilavinod',
-            'uid': '5438123',
-            'fname': 'Oormila',
-            'lname': 'Vinod',
-            'fullname': 'Oormila Vinod',
-            'github': 'oormilavinod'
         },
         {
             'username': 'Minh Nguyen',
@@ -568,46 +235,6 @@ module.exports = {
             'lname': 'Hofmann',
             'fullname': 'Daniel Hofmann',
             'github': 'daniel-j-h'
-        },
-        {
-            'username': 'ranjithjoy',
-            'uid': '5623648',
-            'fname': 'Ranjith',
-            'lname': 'Joy',
-            'fullname': 'Ranjith Joy',
-            'github': 'ranjithjoy'
-         },
-         {
-           'username': 'TeresaPeteti',
-           'uid': '5648480',
-           'fname': 'Peteti',
-           'lname': 'Teresa',
-           'fullname': 'Peteti Chanda Teresa',
-           'github': 'teresapeteti'
-         },
-         {
-           'username': 'Teesta',
-           'uid': '6443638',
-           'fname': 'Teesta',
-           'lname': 'Sinha Ray',
-           'fullname': 'Teesta Sinha Ray',
-           'github': 'teestasr'
-        },
-        {
-           'username': 'Iqhra',
-           'uid': '6371645',
-           'fname': 'Iqhra',
-           'lname': 'Aiman',
-           'fullname': 'Iqhra Aiman',
-           'github': 'iqhra19'
-         },
-         {
-           'username': 'venkanna37',
-           'uid': '5680733',
-           'fname': 'Venkanna',
-           'lname': 'Guthula',
-           'fullname': 'Venkanna Guthula',
-           'github': 'venkanna37'
         },
         {
             'username': 'lmcslssslsssl',
